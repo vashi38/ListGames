cf86b1a352e37193eabf73ee8a0e64ffbdc8b9ee
Shivanand S Sonnad <Shivanand S Sonnad>
2017-02-06 18:57:37 +0530

    added signin and signup fuctionality

--- backend/app.js
+++ backend/app.js
@@ -1,70 +1,68 @@
-var express = require('express');
-var mysql      = require('mysql');
-var app = express();
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-// app.use(bodyParser.json());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+var express    = require("express");
+ var mysql      = require('mysql');
+ var bodyParser = require('body-parser');
+ var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+ var connection = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : '',
+   database : 'test'
+ });
+ var app = express();
+
+ app.use(bodyParser.json());
+ app.use(function(req, res, next) {
+   next();
+ });
+connection.connect(function(err){
+ if(!err) {
+     console.log("Database is connected ... \n\n");
+ } else {
+     console.log("Error connecting database ... \n\n");
+ }
+ });
+
+ app.get("/",function(req,res){
+ connection.query('SELECT * from `table 8`', function(err, rows, fields) {
+ connection.end();
+ res.send(rows);
+   if (!err)
+     console.log('The solution is: ', rows);
+   else
+     console.log('Error while performing Query.');
+   });
+
+ });
+ app.post('/login', urlencodedParser, function(req,res){
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    username = req.body.username;
+    password = req.body.password;
+    connection.query('SELECT * from `users` where username = "'+username+'" ', function(err, rows, fields) {
+    connection.end();
+    res.send(rows);
+     if (!err)
+        console.log('The solution is: ', rows);
+     else
+        console.log('Error while performing Query.');
+     });
 });
 
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'test'
-});
-connection.connect();
-connection.query("SELECT * from `table 8` order by score", function (error, results) {
-  if (error) throw error;
-  else {
-      rows = results;
-  }
-});
-
-
-
-
-// app.use(bodyParser());
-app.get('/',function(req, res){
-
-      console.log(rows);
-      res.send(rows);
+app.post('/signup', urlencodedParser, function(req, res){
+   fullname = req.body.fullName;
+   username = req.body.username;
+   password = req.body.password;
+   connection.query('INSERT INTO users values("'+username+'","'+password+'","'+fullname+'")', function(err, rows, fields) {
+   connection.end();
+   res.send(rows);
+    if (!err)
+       console.log('The solution is: ', rows);
+    else
+       console.log('Error while performing Query.');
     });
 
-
-
-
-app.post('/login', urlencodedParser,function (req, res){
-  connection.query("SELECT * from `users` where username = "+req.body.username+"", function (error, results) {
-    if (error) throw error;
-    else {
-        res.send(row);
-        console.log(row);
-    }
 });
 
-res.send(req.body);
-});
-
-app.post('/addUser', urlencodedParser, function (req, res){
-  connection.query("SELECT * from `users` where username = "+req.body.username+"", function (error, results) {
-    if (error) throw error;
-    else {
-        res.send(row);
-        console.log(row);
-    }
-});
-
-res.send(req.body);
-});
-
-
-connection.end();
-
-app.listen(3000, function(){
-  console.log('Example app listening on port 3000');
-});
+ app.listen(3000);
